91ddfe35a4dd22904c91c24c10a7718717a7e7c6
AslinurTolga <aslinurtolga@gmail.com>
2022-11-25 02:48:16 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import AppRouter from "./router/AppRouter";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  );
+}
+export default App;
